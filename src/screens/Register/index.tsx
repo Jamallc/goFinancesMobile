81339a371e9d64e0470e1006758f7820f90ef152
Jamal <weberrocha15@gmail.com>
2022-05-30 22:40:44 -0300

    Cadastre of pay and buy

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from '../../components/forms/Button';
 import { Input } from '../../components/forms/Input';
-import { Container, Form, Header, Title } from './styles';
+import { TransactionTypeButton } from '../../components/forms/TransactionTypeButton';
+import { Container, Fields, Form, Header, Title, TransactionTypes } from './styles';
 
 
 export function Register() {
+  const [transactionType, setTransactionType] = useState("")
+
+  function handleTransactionTypeSelect(type: 'up' | 'down') {
+    setTransactionType(type)
+  }
+
   return (
     <Container>
       <Header>
         <Title>Cadastro</Title>
       </Header>
 
-    <Form>
-      <Input
-        placeholder='Nome'
-      />
-      <Input
-        placeholder='Preço'
-      />
-    </Form>
+      <Form>
+        <Fields>
+          <Input
+            placeholder='Nome'
+          />
+          <Input
+            placeholder='Preço'
+          />
+          <TransactionTypes>
+            <TransactionTypeButton
+              type='up'
+              title="Income"
+              onPress={() => handleTransactionTypeSelect("up")}
+              isActive={transactionType === 'up'}
+            />
+            <TransactionTypeButton
+              type='down'
+              title="Outcome"
+              onPress={() => handleTransactionTypeSelect("down")}
+              isActive={transactionType === 'down'}
+            />
+          </TransactionTypes>
+        </Fields>
+
+        <Button title='Enviar' />
+      </Form>
     </Container>
   )
 }
